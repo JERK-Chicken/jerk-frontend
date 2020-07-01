afb0af00a43172ba1b27b81d74986249eabc139b
ThirdStockIII <thirdcomstock@gmail.com>
2020-06-30 19:03:48 -0700

    check out our loading gif Logan!

--- src/components/home-header-component.js
+++ src/components/home-header-component.js
@@ -3,13 +3,27 @@ import React from "react";
 
 const Header = () => {
     return (
-        <nav class="navbar navbar-expand-md bg-warning navbar-light card-header">
-            <a class="navbar-brand" href="/"><img src="clearjerklogo.png" alt="logo" width="150" height="60"></img></a>
-            {/* <ul className="navbar-nav mr-auto">
-                
-            </ul> */}
+        <nav className="navbar navbar-expand-md bg-warning navbar-light card-header">
+            <a className="navbar-brand" href="/"><img src="clearjerklogo.png" alt="logo" width="150" height="60"></img></a>
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <p>Login</p>
+                </li>
+            </ul>
         </nav>
     );
 };
 
 export default Header;
+
+{/* <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+    <ul className="navbar-nav mr-auto">
+    </ul>
+</div>
+<div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+    <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+            <LogoutForm></LogoutForm>
+        </li>
+    </ul>
+</div> */}
