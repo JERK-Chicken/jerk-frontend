2fe567d18f97d371db7bd58ea0ce21182975e8fc
logantner <logangantner@gmail.com>
2020-07-06 22:39:57 -0400

    separated homepage footer into new component

--- src/components/home/home-component.js
+++ src/components/home/home-component.js
@@ -1,21 +1,9 @@
 import React from "react";
+import Footer from "./footer";
 
 const Home = () => {
     return (
-    <div>   
-        <div id = "Search">
-        </div>
-        <div id = "Ingredient-Add">
-        </div>
-        <div id = "Ingredient-Table">
-        </div>
-        <div id = "Search Recipe">
-        </div>
-        <div id = "limit">
-        </div>
-        <div id = "Recipe-Results">
-        </div>
-    </div>
+        <Footer/>
     );
 };
 
