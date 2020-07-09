95907f555c899413db938b16e37cd6f1e5714844
Eric Last <elast27@yahoo.com>
2020-07-09 00:30:55 -0400

    recipe page style

--- src/components/recipe-page/recipe-page.js
+++ src/components/recipe-page/recipe-page.js
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function RecipePage() {
+async function getRecipe(setRecipe, id){
+  const res = await axios.get(`/recipes/${id}`)
+  setRecipe(res.data);
+}
 
-  return (
+function RecipePage(props) {
+  const [currRecipe, setRecipe] = useState({});
+  useEffect(()=>{
+    (async_ =>{getRecipe(setRecipe, 5)})();
+  }, []);
+
+  console.log(currRecipe);
+  if(currRecipe.name) return (
     <div className="container">
           <div style={{ marginTop: 20 }}>
             <div className="card">
                 <div className="card-header"><h3>View Recipe</h3></div>
                 <div className="card-body">
-                    <p>Hello Clarice</p>
+                    <div className="row">
+                      <div className="col-6">
+                        Recipe Name
+                      </div>
+                      <div className="col-3">
+                        Prep Time
+                      </div>
+                      <div className="col-3">
+                        Cook Time
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-6">
+                        {currRecipe.name}
+                      </div>
+                      <div className="col-3">
+                        {currRecipe.prepTime}
+                      </div>
+                      <div className="col-3">
+                        {currRecipe.cookTime}
+                      </div>
+                    </div>
+                    <br/>
+                    <div className="row">
+                      <div className="col-6">
+                        <ul>
+                        {currRecipe.ingredients.map((i)=>{
+                            return <li>{i.qty} {i.unit.longType} {i.name}</li>
+                        })}
+                        </ul>
+                      </div>
+                      <div className="col-6">
+                        <ol>
+                        {currRecipe.steps.map((s)=>{
+                          return <li>{s.instruction}</li>
+                        })}
+                        </ol>
+                      </div>
+                    </div>
                 </div>
                 <div className="row card-footer justify-content-between">
                     <button type="button" className="btn btn-success">Save Recipe</button>
@@ -18,6 +67,7 @@ function RecipePage() {
         </div>
     </div>
   )
+  else return <div></div>
 };
 
 export default RecipePage;
